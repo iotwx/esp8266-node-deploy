3a3d515365d894d2eaef59a3b0f7dc92bf93c370
kmaull <kmaull@ucar.edu>
2019-11-22 14:06:03 -0700

    updated main with additional sensors

--- _main.py
+++ _main.py
@@ -9,24 +9,29 @@ mqtt implementation for circuitpython sucks for the 8266 (e.g. it won't work)!
 
 LUCKILY the simple umqtt client works great and was ported here!
 '''
-import machine
-import binascii
-import board
-import busio
-import gc
-import time
-import ujson
 from umqtt.simple import MQTTClient
+import machine
+import board
+from time import sleep
+from binascii import hexlify
+from busio import I2C
+from gc import collect, threshold, mem_free
+from ujson import load
 
 # dynamically load the remaining device imports
 with open("config.json") as f:
-    config = ujson.load(f)
+    config = load(f)
 
     for device in config['sensors'].keys():
         module = config['sensors'][device]['driver']
         globals()[module] = __import__(module)
         print("imported {}".format(module))
-        
+
+    CLIENT_ID = ''.join([chr(b) for b in hexlify(machine.unique_id())])
+    MEASUREMENT_INTERVAL_SEC = config['measurement_interval_sec']
+
+    collect()        
+
 
 def publish_measurement(node, dt, data, client_id=CLIENT_ID):
     client = MQTTClient(client_id, MQ_BROKER, port=MQ_PORT)
@@ -38,7 +43,7 @@ def publish_measurement(node, dt, data, client_id=CLIENT_ID):
     
     print(b"device: nodemcu/{}\ninstrument: {}\nd: '{}'\nm: {}\nmemory: '<alloc:{} free:{}>'" \
                       .format(client_id, node, dt, data, gc.mem_alloc(), gc.mem_free()))
-    gc.collect()
+    collect()
 
 
 def get_current_dt():
@@ -49,7 +54,7 @@ def get_current_dt():
 if __name__ == "__main__":
     try:
         start_hour = machine.RTC().datetime()[4]
-        gc.threshold(1024*12)
+        threshold(1024*12)
         
         #     # set device time from network
         #     ntptime.settime()
@@ -59,12 +64,22 @@ if __name__ == "__main__":
         MQ_BROKER = config['mqtt']['broker']
         MQ_PORT = config['mqtt']['port']
         INSTALLED_SENSORS = list(config['sensors'].keys())
-        CLIENT_ID = ''.join([chr(b) for b in binascii.hexlify(machine.unique_id())])
-        MEASUREMENT_INTERVAL_SEC = config['measurement_interval_sec']
 
+        # set up i2c bus
+        i2c = I2C(board.SCL, board.SDA)
+        
+        collect()
+        
+        if "sgp30" in INSTALLED_SENSORS:
+            sensor = adafruit_sgp30.Adafruit_SGP30(i2c)
+            sensor.iaq_init()
+            sensor.set_iaq_baseline(0x8973, 0x8aae)
+            collect()
+            
+        print(mem_free())
         while True:
             for device in INSTALLED_SENSORS:
-                i2c = busio.I2C(board.SCL, board.SDA)
+                i2c = I2C(board.SCL, board.SDA)
 
                 if device == "bmp280":
                     sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
@@ -73,7 +88,7 @@ if __name__ == "__main__":
                     publish_measurement("{}/temp".format(device), dt, sensor.temperature)
                     publish_measurement("{}/pressure".format(device), dt, sensor.pressure)
                     
-                    gc.collect()
+                    collect()
                     
                 if device == "htu21d":
                     sensor = adafruit_htu21d.HTU21D(i2c)
@@ -82,7 +97,7 @@ if __name__ == "__main__":
                     publish_measurement("{}/temp".format(device), dt, sensor.temperature)
                     publish_measurement("{}/humidity".format(device), dt, sensor.pressure)
                     
-                    gc.collect()
+                    collect()
 
                 if device == "bme680":
                     sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c, address=118)
@@ -93,106 +108,52 @@ if __name__ == "__main__":
                     publish_measurement("{}/pressure", dt, sensor.pressure)
                     publish_measurement("{}/gas", dt, sensor.gas)
                     publish_measurement("{}/altitude", dt, sensor.altitude)
-                    gc.collect()
+                    collect()
 
                 if device == "bme280":
-                    sensor = adafruit_bme280.Adafruit_BME280_I2C(i2c)
+                    sensor = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=118)
                     dt = get_current_dt()
                     
                     publish_measurement("{}/temp", dt, sensor.temperature)
                     publish_measurement("{}/humidity", dt, sensor.humidity)
                     publish_measurement("{}/pressure", dt, sensor.pressure)
                     publish_measurement("{}/altitude", dt, sensor.altitude)
-                    gc.collect()
+                    collect()
                 
                 if device == "mcp9808":
                     sensor = adafruit_mcp9808.MCP9808(i2c_bus)
                     dt = get_current_dt()
                     
                     publish_measurement("{}/temp", dt, sensor.temperature)
-                    gc.collect()
+                    collect()
 
                 if device == "veml6070":
                     sensor = adafruit_veml6070.VEML6070(i2c)
                     dt = get_current_dt()
 
                     publish_measurement("{}/gas", dt, sensor.read)
-                    gc.collect()
+                    collect()
+
+                if device == "sgp30":
+                    sensor = adafruit_sgp30.Adafruit_SGP30(i2c)
+
+                    dt = get_current_dt()
+
+                    publish_measurement("{}/eCO2", dt, sensor.eCO2)
+                    publish_measurement("{}/TVOC", dt, sensor.TVOC)
+                    collect()
                     
-            time.sleep(MEASUREMENT_INTERVAL_SEC)
+                    print("**** Baseline values: eCO2 = {0x%x}, TVOC = {0x%x}"
+                          .format(sgp30.baseline_eCO2, sgp30.baseline_TVOC))
+
+
+
+            sleep(MEASUREMENT_INTERVAL_SEC)
 
             # reset the chip after 3 hours
             if machine.RTC().datetime()[4] > start_hour+2:
                  machine.reset()
-# 
-#     
-# 
-# 
-# try:     
-#     MEASUREMENT_INTERVAL_SEC = 60
-#     machine.RTC().datetime((2019, 10, 6, 11, 0, 0, 0, 0))
-# 
-#     i2c = busio.I2C(board.SCL, board.SDA)
-# 
-#     # connect to MQ broker
-#     MQ_BROKER, MQ_PORT = 'pt2050dashboard-dev.tacc.utexas.edu', 1883
-#     client_id = ''.join([chr(b) for b in binascii.hexlify(machine.unique_id())])
-#     client = MQTTClient(client_id, MQ_BROKER, port=MQ_PORT)
-#     client.connect()
-#     
-#     gc.threshold(1024*12)
-#     
-#     # loop over measurements
-#     start_hour = machine.RTC().datetime()[4]
-#     
-#     while True:
-#         y, m, dd, z, hh, mm, ss, _ = machine.RTC().datetime()
-#         dt = "{}-{:02d}-{:02d}T{:02d}:{:02d}:{:02d}".format(y, m, dd, hh, mm, ss)
-# 
-#         # bme680
-#         sensor = adafruit_bme680.Adafruit_BME680_I2C(i2c, address=118)
-#         
-#         publish_measurement(client, client_id, "bme680/temp", dt, sensor.temperature)
-#         publish_measurement(client, client_id, "bme680/humidity", dt, sensor.humidity)
-#         publish_measurement(client, client_id, "bme680/pressure", dt, sensor.pressure)
-#         publish_measurement(client, client_id, "bme680/gas", dt, sensor.gas)
-#         publish_measurement(client, client_id, "bme680/altitude", dt, sensor.altitude)
-# 
-#         # bmp280
-#         
-#         # htu21d
-#         
-#         # mcp9808
-#         
-#         # veml6070
-# 
-# #         client.publish("measurements",
-# #                         b"device: nodemcu/{}\ninstrument: bme680/temp\nd: '{}'\nm: {}\nmemory: '<alloc:{} free:{}>'" \
-# #                           .format(client_id, dt, sensor.temperature, gc.mem_alloc(), gc.mem_free()))
-# # 
-# #         client.publish("measurements",
-# #                         b"device: nodemcu/{}\ninstrument: bme680/humidity\nd: '{}'\nm: {}\nmemory: '<alloc:{} free:{}>'" \
-# #                           .format(client_id, dt, sensor.humidity, gc.mem_alloc(), gc.mem_free()))
-# # 
-# #         client.publish("measurements",
-# #                         b"device: nodemcu/{}\ninstrument: bme680/pressure\nd: '{}'\nm: {}\nmemory: '<alloc:{} free:{}>'" \
-# #                           .format(client_id, dt, sensor.pressure, gc.mem_alloc(), gc.mem_free()))
-# # 
-# #         client.publish("measurements",
-# #                         b"device: nodemcu/{}\ninstrument: bme680/gas\nd: '{}'\nm: {}\nmemory: '<alloc:{} free:{}>'" \
-# #                           .format(client_id, dt, sensor.gas, gc.mem_alloc(), gc.mem_free()))
-# # 
-# #         client.publish("measurements",
-# #                         b"device: nodemcu/{}\ninstrument: bme680/altitude\nd: '{}'\nm: {}\nmemory: '<alloc:{} free:{}>'" \
-# #                           .format(client_id, dt, sensor.altitude, gc.mem_alloc(), gc.mem_free()))
-# 
-#         time.sleep(MEASUREMENT_INTERVAL_SEC)
-#         
-#         # reset the machine every 2 hours
-#         if machine.RTC().datetime()[4] > start_hour+2:
-#             machine.reset()
-# 
     except Exception as e:
         print("[fail] {}".format(e))
-        time.sleep(MEASUREMENT_INTERVAL_SEC / 10.)
+        sleep(MEASUREMENT_INTERVAL_SEC / 10.)
         machine.reset()
